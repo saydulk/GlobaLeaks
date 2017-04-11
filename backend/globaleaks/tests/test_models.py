40575c3d199886016177620103353842eea10b7a
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-04-11 19:14:43 +0200

    Refactored test helpers adding possibility to check for models count upon creation/delation/etc

--- backend/globaleaks/tests/test_models.py
+++ backend/globaleaks/tests/test_models.py
@@ -12,7 +12,6 @@ from globaleaks.models.l10n import NodeL10NFactory, NotificationL10NFactory, Ena
 from globaleaks.orm import transact
 from globaleaks.tests import helpers
 
-
 class TestSystemConfigModels(helpers.TestGL):
     @transact
     def _test_config_import(self, store):
@@ -118,19 +117,6 @@ class TestModels(helpers.TestGL):
     receiver_inc = 0
 
     @transact
-    def context_add(self, store):
-        c = self.localization_set(self.dummyContext, models.Context, 'en')
-        context = models.Context(c)
-        context.tid = app_state.root_id
-        context.questionnaire_id = db_get_default_questionnaire_id(store)
-        context.tip_timetolive = 1000
-        context.description = context.name = \
-            context.submission_disclaimer = \
-            context.submission_introduction = {'en': 'Localized723'}
-        store.add(context)
-        return context.id
-
-    @transact
     def receiver_add(self, store):
         u = self.localization_set(self.dummyReceiverUser_1, models.User, 'en')
         receiver_user = models.User(u)
@@ -155,56 +141,6 @@ class TestModels(helpers.TestGL):
         self.receiver_inc += 1
 
         return receiver.id
-
-    @transact
-    def create_context_with_receivers(self, store):
-        u1 = self.localization_set(self.dummyReceiverUser_1, models.User, 'en')
-        receiver_user1 = models.User(u1)
-        receiver_user1.password = self.dummyReceiverUser_1['password']
-        receiver_user1.salt = self.dummyReceiverUser_1['salt']
-
-        u2 = self.localization_set(self.dummyReceiverUser_2, models.User, 'en')
-        receiver_user2 = models.User(u2)
-        receiver_user2.password = self.dummyReceiverUser_2['password']
-        receiver_user2.salt = self.dummyReceiverUser_2['salt']
-
-        store.add(receiver_user1)
-        store.add(receiver_user2)
-
-        c = self.localization_set(self.dummyContext, models.Context, 'en')
-        context = models.Context(c)
-        context.tid = app_state.root_id
-        context.questionnaire_id = db_get_default_questionnaire_id(store)
-        context.tip_timetolive = 1000
-        context.description = context.name = \
-            context.submission_disclaimer = \
-            context.submission_introduction = {'en': 'Localized76w'}
-
-        r1 = self.localization_set(self.dummyReceiver_1, models.Receiver, 'en')
-        r2 = self.localization_set(self.dummyReceiver_2, models.Receiver, 'en')
-        receiver1 = models.Receiver(r1)
-        receiver2 = models.Receiver(r2)
-
-        receiver1.user = receiver_user1
-        receiver2.user = receiver_user2
-
-        receiver1.user.pgp_key_expiration = "1970-01-01 00:00:00.000000"
-        receiver1.user.pgp_key_fingerprint = ""
-        receiver1.user.pgp_key_public = ""
-
-        receiver2.user.pgp_key_expiration = "1970-01-01 00:00:00.000000"
-        receiver2.user.pgp_key_fingerprint = ""
-        receiver2.user.pgp_key_public = ""
-
-        receiver1.mail_address = 'x@x.it'
-        receiver2.mail_address = 'x@x.it'
-
-        context.receivers.add(receiver1)
-        context.receivers.add(receiver2)
-
-        store.add(context)
-
-        return context.id
 
     @transact
     def create_receiver_with_contexts(self, store):
@@ -246,11 +182,6 @@ class TestModels(helpers.TestGL):
         return receiver.id
 
     @transact
-    def list_receivers_of_context(self, store, context_id):
-        context = models.Context.db_get(store, id=context_id)
-        return [r.id for r in context.receivers]
-
-    @transact
     def list_context_of_receivers(self, store, receiver_id):
         """
         Return the list of context ids associated with the receiver identified
@@ -260,44 +191,36 @@ class TestModels(helpers.TestGL):
         return [c.id for c in receiver.contexts]
 
     @inlineCallbacks
-    def test_context_add_and_get(self):
-        context_id = yield self.context_add()
-        check = yield models.Context.test(id=context_id)
-        self.assertTrue(check)
-
-    @inlineCallbacks
-    def test_context_add_and_del(self):
-        context_id = yield self.context_add()
-        yield models.Context.delete(id=context_id)
-        check = yield models.Context.test(id=context_id)
-        self.assertFalse(check)
-
-    @inlineCallbacks
-    def test_receiver_add_and_get(self):
-        receiver_id = yield self.receiver_add()
-        check = yield models.Receiver.test(id=receiver_id)
-        self.assertTrue(check)
-
-    @inlineCallbacks
     def test_receiver_add_and_del(self):
         receiver_id = yield self.receiver_add()
-        yield models.Receiver.delete(id=receiver_id)
-        check = yield models.Receiver.test(id=receiver_id)
+
+        yield self.check_entries_count({
+          'Receiver': 1,
+          'User': 1
+        })
+
+        yield models.User.delete(id=receiver_id)
+        check = yield models.User.test(id=receiver_id)
         self.assertFalse(check)
 
-    @inlineCallbacks
-    def test_context_receiver_reference_1(self):
-        context_id = yield self.create_context_with_receivers()
-        yield self.assert_model_exists(models.Context, id=context_id)
-        receivers = yield self.list_receivers_of_context(context_id)
-        self.assertEqual(2, len(receivers))
+        yield self.check_entries_count({
+          'Receiver': 0,
+          'User': 0
+        })
 
     @inlineCallbacks
-    def test_context_receiver_reference_2(self):
+    def test_receiver_context_reference(self):
         receiver_id = yield self.create_receiver_with_contexts()
         yield self.assert_model_exists(models.Receiver, id=receiver_id)
         contexts = yield self.list_context_of_receivers(receiver_id)
         self.assertEqual(2, len(contexts))
+
+        yield self.check_entries_count({
+          'Context': 2,
+          'Receiver': 1,
+          'Receiver_Context': 2,
+          'User': 1
+        })
 
 
 class TestField(helpers.TestGL):
@@ -321,9 +244,27 @@ class TestField(helpers.TestGL):
         field1_id = yield self.create_dummy_field()
         yield self.assert_model_exists(models.Field, id=field1_id)
 
+        yield self.check_entries_count({
+          'Field': 1,
+          'Question': 1,
+          'Question_Tenant': 1
+        })
+
         field2_id = yield self.create_dummy_field(type='checkbox')
         yield self.assert_model_exists(models.Field, id=field2_id)
+
+        yield self.check_entries_count({
+          'Field': 2,
+          'Question': 2,
+          'Question_Tenant': 2
+        })
 
         yield models.Field.delete(id=field1_id)
         yield self.assert_model_not_exists(models.Field, id=field1_id)
         yield self.assert_model_exists(models.Field, id=field2_id)
+
+        yield self.check_entries_count({
+          'Field': 1,
+          'Question': 1,
+          'Question_Tenant': 1
+        })
