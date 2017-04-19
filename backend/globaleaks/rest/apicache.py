cee061a0df92df5ee7b39763e631679c00d953d0
Giovanni Pellerano <giovanni.pellerano@evilaliv3.org>
2017-04-19 22:26:00 +0200

    Implemented cache callback chain

--- backend/globaleaks/rest/apicache.py
+++ backend/globaleaks/rest/apicache.py
@@ -1,4 +1,6 @@
-from twisted.internet.defer import inlineCallbacks, returnValue
+# -*- coding: UTF-8
+
+from twisted.internet.defer import inlineCallbacks, returnValue, Deferred
 
 
 class GLApiCache(object):
@@ -10,42 +12,42 @@ class GLApiCache(object):
         """
         Get a resource from cache.
         The function is written to be run in main twisted thread,
-        guaranee of no concurrency.
+        guarantee of no concurrency.
         """
-        try:
-            returnValue(cls._cache[tid][resource][language])
-        except KeyError:
+        if tid not in cls._cache:
+            cls._cache[tid] = {}
+
+        if resource not in cls._cache[tid]:
+            cls._cache[tid][resource] = {}
+
+        if language not in cls._cache[tid][resource]:
+            deferred = Deferred()
+            cls._cache[tid][resource][language] = deferred
             value = yield function(*args, **kwargs)
+            cls._cache[tid][resource][language] = value
+            deferred.callback(None)
+        else:
+            if isinstance(cls._cache[tid][resource][language], Deferred):
+                yield cls._cache[tid][resource][language]
 
-            if tid not in cls._cache:
-                cls._cache[tid] = {}
+        returnValue(cls._cache[tid][resource][language])
 
-            if resource not in cls._cache[tid]:
-                cls._cache[tid][resource] = {}
-
-            if language not in cls._cache[tid][resource]:
-                cls._cache[tid][resource][language] = value
-
-            returnValue(cls._cache[tid][resource][language])
 
     @classmethod
     def invalidate(cls, tid=None, resource=None, language=None):
         """
         Invalidate cache.
         The function is written to be run in main twisted thread,
-        guaranee of no concurrency.
+        guarantee of no concurrency.
         """
-        try:
-            if language is not None and resource is not None and tid is not None:
-                del cls._cache[tid][resource][language]
+        if language is not None:
+            del cls._cache[tid][resource][language]
 
-            elif resource is not None and tid is not None:
-                del cls._cache[tid][resource]
+        elif resource is not None:
+            del cls._cache[tid][resource]
 
-            elif tid is not None:
-                del cls._cache[tid]
+        elif tid is not None:
+            del cls._cache[tid]
 
-            else:
-                cls._cache.clear()
-        except KeyError:
-            pass
+        else:
+            cls._cache.clear()
